372d7f8a9b4fb5a39b8d8ad2a52310ea62a747a2
Jess Lee <jessleenyc@gmail.com>
2016-03-09 17:39:33 -0500

    receiving texts

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,10 @@ class MessagesController < ActionController::Base
 
   def index
     puts params["Body"]
+    @gratitude = Gratitude.create({message: params["Body"], message_date: Date.now})
+    @user = User.find_by({id: 1})
+    @user.gratitudes.push(@gratitude)
   end
-  
+
+
 end
