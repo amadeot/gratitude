5e941d72ce519f267666d0e82031d963384bc3da
Jess Lee <jessleenyc@gmail.com>
2016-03-10 14:56:57 -0500

    testing db connection

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,9 +2,14 @@ class MessagesController < ActionController::Base
 
   def index
     puts params["Body"]
-    @gratitude = Gratitude.create({message: params["Body"], message_date: Date.now})
-    @user = User.find_by({id: 1})
+    # @gratitude = Gratitude.create({message: params["Body"], message_date: Date.now})
+    @test_gratitude_1 = Gratitude.create({message: "test message", message_date: Date.now})
+    @test_gratitude_2 = Gratitude.create({message: "testy", message_date: "2016-03-09"})
+    @user = User.first
     @user.gratitudes.push(@gratitude)
+    @user.gratitudes.push(@test_gratitude_1)
+    @user.gratitudes.push(@test_gratitude_2)
+    @user.save
   end
 
 
