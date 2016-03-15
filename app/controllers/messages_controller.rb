0eace6888e64d346145abd4eb12178551fa45b88
Jess Lee <jessleenyc@gmail.com>
2016-03-15 14:30:04 -0400

    adds authenticity token

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,12 @@
 class MessagesController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
+  
   def index
     puts 'LOG ANYTHING'
     puts 'Is this loggin' + params["Body"]
     # @gratitude = Gratitude.create({message: params["Body"], message_date: "2016-03-03"})
-    @test_gratitude_2 = Gratitude.create({message: "day 3", message_date: "2016-03-15"})
+    @test_gratitude_2 = Gratitude.create({message: "put back authenticity token", message_date: "2016-03-15"})
     @user = User.first
     @user.gratitudes.push(@test_gratitude_2)
     @user.save
