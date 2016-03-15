44e020d776abc51e7fd363ec89391bed1805a50d
Jess Lee <jessleenyc@gmail.com>
2016-03-15 15:25:19 -0400

    updates params body

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
     puts 'LOG ANYTHING'
     puts 'Is this loggin' + params["Body"]
     # @gratitude = Gratitude.create({message: params["Body"], message_date: "2016-03-03"})
-    @test_gratitude_2 = Gratitude.create({message: "put back authenticity token", message_date: "2016-03-15"})
+    @test_gratitude_2 = Gratitude.create({message: params["Body"], message_date: "2016-03-15"})
     @user = User.first
     @user.gratitudes.push(@test_gratitude_2)
     @user.save
