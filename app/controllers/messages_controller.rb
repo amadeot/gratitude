8e7db0447949dc486b8add0c18c896ccb2d747ea
Jess Lee <jessleenyc@gmail.com>
2016-03-15 13:03:15 -0400

    adds redirect

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,10 +3,12 @@ class MessagesController < ApplicationController
   def index
     puts 'Is this loggin' + params["Body"]
     # @gratitude = Gratitude.create({message: params["Body"], message_date: "2016-03-03"})
-    @test_gratitude_2 = Gratitude.create({message: "testy", message_date: "2016-03-09"})
+    @test_gratitude_2 = Gratitude.create({message: "day 3", message_date: "2016-03-15"})
     @user = User.first
     @user.gratitudes.push(@test_gratitude_2)
     @user.save
+
+    redirect_to "/"
   end
 
 
