5cd339b983262396d95621edfc6fa3b7f1cfe20c
Jess Lee <jessleenyc@gmail.com>
2016-03-15 15:14:09 -0400

    remove authenticity token

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,5 @@
 class MessagesController < ApplicationController
 
-  skip_before_action :verify_authenticity_token
-  
   def index
     puts 'LOG ANYTHING'
     puts 'Is this loggin' + params["Body"]
