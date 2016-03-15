a8b38d73944bcda7e96621da32e08e78681d58ce
Jess Lee <jessleenyc@gmail.com>
2016-03-15 13:56:41 -0400

    puts

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
 
   def index
+    puts 'LOG ANYTHING'
     puts 'Is this loggin' + params["Body"]
     # @gratitude = Gratitude.create({message: params["Body"], message_date: "2016-03-03"})
     @test_gratitude_2 = Gratitude.create({message: "day 3", message_date: "2016-03-15"})
@@ -8,7 +9,7 @@ class MessagesController < ApplicationController
     @user.gratitudes.push(@test_gratitude_2)
     @user.save
 
-    SMSLogger.log_text_message from_number, message_body
+    # SMSLogger.log_text_message from_number, message_body
 
     redirect_to "/"
   end
