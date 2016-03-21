35ea59595db1f2aebc40a89966a30a7fd3535563
Jess Lee <jessleenyc@gmail.com>
2016-03-21 18:07:16 -0400

    adds logger

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,10 +3,10 @@ class MessagesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    puts 'LOG ANYTHING'
+    Rails.logger.info 'LOG ANYTHING'
     puts 'Is this loggin' + params["Body"]
     # @gratitude = Gratitude.create({message: params["Body"], message_date: "2016-03-03"})
-    @test_gratitude_2 = Gratitude.create({message: params["Body"], message_date: "2016-03-15"})
+    @test_gratitude_2 = Gratitude.create({message: params["Body"], message_date: Date.today})
     @user = User.first
     @user.gratitudes.push(@test_gratitude_2)
     @user.save
